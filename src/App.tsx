3f69fb32e5774370a2e088a39cc10b7a958b43fe
Alexander Wilts <alexander.wilts@codecentric.de>
2020-04-13 18:06:46 +0200

    made app ready for hosting in gcp app engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const queryUrl = "https://composed-falcon-273907.ey.r.appspot.com/backend/message"
+    axios.get(queryUrl)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+  } )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          lolnoob
         </p>
         <a
           className="App-link"
