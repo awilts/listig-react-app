8887df5b3864e7312d95389ef57ab4be685b06ff
Alexander Wilts <alexanderwilts@googlemail.com>
2020-05-08 14:42:48 +0200

    added mock heavy tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,24 +2,25 @@ import React from 'react'
 import App from './App'
 import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { subscribeToItems } from './actions/webSocketActions'
+import { axiosGetItems } from './actions/axiosGetItemsActions'
+import ItemList from './components/ItemList'
+import CreateItemForm from './components/CreateItemForm'
+import ClearItemsButton from './components/ClearItemsButton'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-jest.mock('./components/ItemList', () => 'div')
-jest.mock('./components/CreateItemForm', () => 'div')
-jest.mock('./components/ClearItemsButton', () => 'div')
 jest.mock('react-redux')
-
-import { subscribeToItems } from './actions/webSocketActions'
-import { axiosGetItems } from './actions/axiosGetItemsActions'
-
+jest.mock('./components/ItemList', () => jest.fn().mockReturnValue(''))
+jest.mock('./components/CreateItemForm', () => jest.fn().mockReturnValue(''))
+jest.mock('./components/ClearItemsButton', () => jest.fn().mockReturnValue(''))
 jest.mock('./actions/axiosGetItemsActions', () => ({
     axiosGetItems: jest.fn().mockReturnValue(jest.fn()),
 }))
 jest.mock('./actions/webSocketActions', () => ({
     subscribeToItems: jest.fn().mockReturnValue(jest.fn()),
 }))
-describe('App init', () => {
+describe('On initialization App ', () => {
     mount(<App />)
     it('loads items', () => {
         expect(axiosGetItems).toHaveBeenCalledTimes(1)
@@ -28,6 +29,12 @@ describe('App init', () => {
         expect(subscribeToItems).toHaveBeenCalledTimes(1)
     })
     it('renders ItemList', () => {
-        // expect(mock).toHaveBeenCalledTimes(1)
+        expect(ItemList).toHaveBeenCalledTimes(1)
+    })
+    it('renders CreateItemForm', () => {
+        expect(CreateItemForm).toHaveBeenCalledTimes(1)
+    })
+    it('renders ClearItemsButton', () => {
+        expect(ClearItemsButton).toHaveBeenCalledTimes(1)
     })
 })
